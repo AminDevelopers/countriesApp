a0e85f45b301820f568178086ee0af9e4fd6fd4f
amin <aminledragon@gmail.com>
2024-02-04 20:20:30 +0100

    last update

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Main.css";
 import { Outlet } from "react-router-dom";
 
 export default function Main() {
-  const [dark, setDark] = useState(true);
-  const color = dark ? { color: "black" } : { color: "hsl(0, 0%, 98%)" };
-
-  const handleDarkClick = () => {
-    setDark(!dark);
-  };
-
   return (
     <div className="Main">
       <div className="navbar">
         <h2>where in the world ?</h2>
         <div className="darkMode">
-          <ion-icon style={color} name="moon-outline"></ion-icon>
-          <p onClick={handleDarkClick}>dark mode</p>
+          <ion-icon name="moon-outline"></ion-icon>
+          <p>dark mode</p>
         </div>
       </div>
       <div>
